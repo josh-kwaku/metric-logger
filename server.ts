161e2fd6f5711ee63b1cc90f637f01ba1feb00db
Kwaku-Boateng Joshua <Joshuakwaku@outlook.com>
2020-04-20 21:03:29 +0100

    Update server.ts

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ import { ErrorHandler } from "./utils/error_handler";
 import { AppError } from './utils/error';
 
 const errorHandler = new ErrorHandler();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 process.on('unhandledRejection', (reason: string, p: Promise<any>) => {
     throw reason;
